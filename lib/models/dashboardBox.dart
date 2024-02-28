384da3be3e1644d870c28c43f72ca1338d0c8710
Skander Mhamdi <skander.mhamdi@etudiant-isi.utm.tn>
2024-02-28 11:40:32 +0100

    admin dashboard

--- lib/models/dashboardBox.dart
+++ lib/models/dashboardBox.dart
@@ -4,31 +4,33 @@ class DashboardBox extends StatelessWidget {
   final String title;
   final int value;
   final Color background; // Add a Color parameter
-  final double height; // Add a height parameter
 
-  DashboardBox({required this.title, required this.value, required this.background, required this.height});
+  DashboardBox({required this.title, required this.value, required this.background});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      child: Container(
-        height: height, // Set the height of the container
-        color: background,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-              Text(
-                '$value',
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            ],
+    return SizedBox(
+      width: 100,
+      height: 50, // Set the desired height for all boxes
+      child: Card(
+        elevation: 4,
+        child: Container(
+          color: background,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                Text(
+                  '$value',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
       ),
