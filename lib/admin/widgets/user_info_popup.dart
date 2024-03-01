aac7a9f1f882e66d4b22f640f3be592426afa2c9
Yassine Sayari <yassino.sayari@gmail.com>
2024-03-01 10:01:07 +0100

    changing user popup

--- lib/admin/widgets/user_info_popup.dart
+++ lib/admin/widgets/user_info_popup.dart
@@ -14,65 +14,66 @@ class EmployeeInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      elevation: 8.0,
-      content: IntrinsicHeight(
-        child: Container(
-          width: double.infinity,
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ClipOval(
-                    child: Image.network(
-                      "$imageUrl/${employee.image}",
-                      width: 200,
-                      height: 200,
-                      fit: BoxFit.fill,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Image.network(
-                          noImageUrl,
-                          width: 200.0,
-                          height: 200.0,
-                          fit: BoxFit.cover,
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Employee's Details",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  buildInfoRow(Icons.person, "Full Name:", employee.fullName),
-                  buildInfoRow(Icons.people_rounded, "Role:", employee.roles[0]),
-                  buildInfoRow(Icons.email, "Email:", employee.email),
-                  buildInfoRow(Icons.local_fire_department, "Department:", employee.department??"N/A"),                  
-                  buildInfoRow(Icons.phone, "Phone:", employee.phone),
-                  buildInfoRow(Icons.calendar_month, "Hiring Date:",  DateFormat('MMM dd yyyy').format(employee.hiringDate)),
-                  SizedBox(height: 20),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        "Close",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color.fromARGB(255, 20, 91, 150),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      insetPadding: EdgeInsets.all(8.0),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipOval(
+                child: Image.network(
+                  "$imageUrl/${employee.image}",
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.fill,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Image.network(
+                      noImageUrl,
+                      width: 200.0,
+                      height: 200.0,
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
               ),
-            ),
+              SizedBox(height: 20),
+              Text(
+                "Employee's Details",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              buildInfoRow(Icons.person, "Full Name:", employee.fullName),
+              buildInfoRow(Icons.people_rounded, "Role:", employee.roles[0]),
+              buildInfoRow(Icons.email, "Email:", employee.email),
+              buildInfoRow(Icons.local_fire_department, "Department:", employee.department ?? "N/A"),
+              buildInfoRow(Icons.phone, "Phone:", employee.phone),
+              buildInfoRow(Icons.calendar_month, "Hiring Date:", DateFormat('MMM dd yyyy').format(employee.hiringDate)),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    "Close",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color.fromARGB(255, 20, 91, 150),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
