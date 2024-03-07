83d93170c721989f28b53d065af6425fd9eea374
Skander Mhamdi <skander.mhamdi@etudiant-isi.utm.tn>
2024-03-07 22:40:53 +0100

    Admin dashboard icons (all projects, client, team leader, engineer)

--- lib/admin/screens/admin_dashboard.dart
+++ lib/admin/screens/admin_dashboard.dart
@@ -41,35 +41,89 @@ class _AdminDashboardState extends State<AdminDashboard> {
         title: Text('Admin dashboard, user: $userFullName'),
       ),
       drawer:AdminDrawer(selectedRoute: '/admindashboard'),
-        body: GridView.count(
+        body:
+
+        GridView.count(
+  crossAxisCount: 4, // Adjust number for your design
+  children: <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CircleAvatar(
+          child: Icon(Icons.business_center_outlined),
+          backgroundColor: Colors.lightGreen,
+          radius: 30,
+        ),
+        Text('All Projects'),
+        Text('0'),
+      ],
+    ),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CircleAvatar(
+          child: Icon(Icons.group_outlined),
+          backgroundColor: Colors.lightBlue,
+          radius: 30,
+        ),
+        Text('Client'),
+        Text('0'),
+      ],
+    ),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CircleAvatar(
+          child: Icon(Icons.person_4_outlined),
+          backgroundColor: Colors.purple,
+          radius: 30,
+        ),
+        Text('Team Leader'),
+        Text('0'),
+      ],
+    ),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CircleAvatar(
+          child: Icon(Icons.person_outlined),
+          backgroundColor: Colors.orange,
+          radius: 30,
+        ),
+        Text('Engineer'),
+        Text('0'),
+      ],
+    ),
+  ],
+)
+
+
+
+        /*GridView.count(
         crossAxisCount: 2,
           children: [
             DashboardBox(
-              title: 'All project',
+              title: "All project",
               value: 0,
-              background: Colors.green,
+              background: Colors.green[100]!,
               ),
             DashboardBox(
               title: 'Client',
               value: 0,
-              background: Colors.blue,
+              background: Colors.blue[100]!,
               ),
             DashboardBox(
               title: 'Team Leader',
               value: 0,
-              background: Colors.purple,
+              background: Colors.purple[100]!,
               ),
             DashboardBox(
               title: 'Engineer',
               value: 0,
-              background: Colors.orange,
+              background: Colors.orange[100]!,
               ),
           ],
-        ),
-       
-        /* Center(
-        child: Text("admin dashboard"),
-      ),*/
+        ),*/
     );
   }
 }
